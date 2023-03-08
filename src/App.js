cd25d46f7b53edbc642764bf14a94194dd677f16
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-08 17:28:50 -0600

    added state for selectedPage and setSelectedPage, created Navbar component and imported in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import { Navbar } from "./scenes/Navbar";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState("home");
+  // This will use the media query we created isAboveMediumScreens, and it will set the min width to be 1060px.
+  // Determines whether the current broswer size if the min width is less than 1060 or above 1060
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
   return (
-    <div className="App">
-      
+    <div className="app bg-deep-blue">
+      <Navbar 
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   );
 }
