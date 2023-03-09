4ff029fb81d9883709cadad7b69636ddf936d142
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-09 14:06:17 -0600

    added 4 anchor tags in SocialMediaIcons for the links when you click on the icons

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const SocialMediaIcons = () => {
   return (
-    <div>SocialMediaIcons</div>
+    <div className='flex justify-center md:justify-start my-10 gap-7'>
+        <a className="hover:opacity-50 transition duration-500" href=""></a>
+        <a className="hover:opacity-50 transition duration-500" href=""></a>
+        <a className="hover:opacity-50 transition duration-500" href=""></a>
+        <a className="hover:opacity-50 transition duration-500" href=""></a>
+    </div>
   )
 }
 
