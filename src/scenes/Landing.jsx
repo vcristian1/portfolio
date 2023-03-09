ce1e72f94cf7d5e6d1f0f0d9ef17089524d90b72
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-09 13:59:38 -0600

    added additional motion.div to contain the additional anchor links that show contact me and lets talk. Added delay to the second motion.div to give it a smoother feel

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -61,6 +61,38 @@ const Landing = ({setSelectedPage}) => {
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
             </motion.div>
+
+            {/* Call To Action Here */}
+            {/* Delay added to transition to give it a smoother feel */}
+            <motion.div
+             className="flex mt-5 justify-center md:justify-start"
+             initial="hidden"
+             whileInView="visible"
+             viewport={{ once: true, amount:0.5}}
+             transition={{ delay: 0.2, duration: 0.5}}
+             variants={{
+               // Will start -50 from where it originally is located
+               hidden: { opacity: 0, x: -50 },
+               visible: { opacity: 1, x: 0}
+             }}
+            >
+                <AnchorLink
+                 className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold hover:bg-blue hover:text-white transition duration-500"
+                 onClick={() => setSelectedPage("contact")}
+                 href="#contact"
+                >
+                 Contact Me
+                </AnchorLink>
+                <AnchorLink
+                 className="rounded-r-sm bg-gradient-rainblue py-0.5 pr-0.5"
+                 onClick={() => setSelectedPage("contact")}
+                 href="#contact"
+                >
+                 <div className="bg-deep-blue hover:text-red transition duration-500 w-full h-full flex items-center justify-center font-playfair px-10">
+                    Let's talk.
+                 </div>
+                </AnchorLink>
+            </motion.div>
         </div>
     </section>
   )
