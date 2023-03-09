ccf92f50ae05b69a8ad008e25402791aa782fa7d
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-08 19:45:57 -0600

    added semantic element section and gave it an id of home, the id is what the Home AnchorLink will reference once you click

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -2,9 +2,14 @@ import useMediaQuery from "../hooks/useMediaQuery";
 import { motion } from "framer-motion";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
-export const Landing = () => {
+const Landing = ({setSelectedPage}) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width-1060px)");
+
   return (
-    <div>Landing</div>
+    // This is the ID that the home AnchorLink is using to navigate.
+    <section id="home" className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10">
+
+    </section>
   )
 }
 
