a21165a3758ac721e33248f1206fc24237b17f13
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-08 19:51:56 -0600

    added image section in Landing component

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -8,7 +8,19 @@ const Landing = ({setSelectedPage}) => {
   return (
     // This is the ID that the home AnchorLink is using to navigate.
     <section id="home" className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10">
+    
+    {/* Image Section Here */}
+    <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
+        {isAboveMediumScreens ? (
+          <div className="relative">
+            <img src="assets/profile-image.png" alt="profile" />
+          </div>
+        ) : (
+          <div>
 
+          </div>
+        )}
+    </div>
     </section>
   )
 }
