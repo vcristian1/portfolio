828c1d44077fd1be380a121f9264d8a8a2137601
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-10 21:52:27 -0600

    added section to Testimonials, and within section added motion.div to contain the ready to use animations

--- src/scenes/Testimonials.jsx
+++ src/scenes/Testimonials.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import LineGradient from "../components/LineGradient";
 
 export const Testimonials = () => {
   return (
-    <div id="testimonials">Testimonials</div>
+    <section id="testimonials" className="pt-32 pb-16">
+      {/* HEADING */}
+      <motion.div
+        className="md:w-1/3 text-center md:text-left"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, x: -50 },
+          visible: { opacity: 1, x: 0 },
+        }}
+      >
+        <p className="font-playfair font-semibold text-4xl mb-5 text-red">
+          TESTIMONIALS
+        </p>
+        <LineGradient width="mx-auto w-1/5" />
+        <p className="mt-10 text-center">
+          Here's What People are Saying About My Work. Aliquam aliquet integer
+          ut fames odio in at. At magna ornare dictum lectus.
+        </p>
+      </motion.div>
+    </section>
   )
 }
 
