da93480388b206fd9c692f48f1db94f0f6fc7439
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-11 17:57:34 -0600

    added to Contact.jsx specifically the form using React Hook Form

--- src/scenes/Contact.jsx
+++ src/scenes/Contact.jsx
@@ -38,6 +38,71 @@ const Contact = () => {
                 </div>
               </div>
             </motion.div>
+
+            {/* FORM AND IMAGE HERE */}
+            <div className="md:flex md:justify-between gap-16 mt-5">
+                <motion.div
+                className="basis-1/2 mt-10 md:mt-0 flex justify-center"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ duration: 0.5 }}
+                variants={{
+                    hidden: { opacity: 0, y: 50 },
+                    visible: { opacity: 1, y: 0 },
+                }}
+                >
+                  <img src="../assets/contact-image.jpeg" alt="contact" />
+                </motion.div>
+                
+                <motion.div
+                className="basis-1/2 mt-10 md:mt-0"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ delay: 0.2, duration: 0.5 }}
+                variants={{
+                    hidden: { opacity: 0, y: 50 },
+                    visible: { opacity: 1, y: 0 },
+                }}
+                >
+                    <form 
+                    action="" 
+                    target="_blank" 
+                    onSubmit={onSubmit} 
+                    method="POST"
+                    >
+                        <input 
+                        className="w-full bg-blue font-semibold placeholder-opaque-black p-3" 
+                        type="text" 
+                        placeholder="NAME"
+                        // Telling React Hook Form I am Registering this particular input inside of it, and its called name with validation
+                        {...register("name", {
+                            required: true, 
+                            maxLength: 100,
+                        })}
+                         />
+                         {errors.name && (
+                            <p className="text-red mt-1">
+                                {errors.name.type === "required" && "This field is required!"}
+                                {errors.name.type === "max length" && "Max length is 100 characters!"}
+                            </p>  
+                         )}
+
+                         <input  
+                         className="w-full bg-blue font-semibold placeholder-opaque-black p-3 mt-5" 
+                         type="text" 
+                         placeholder="EMAIL"
+                         // Telling React Hook Form I am Registering this particular input inside of it, and its called name with validation
+                         {...register("email", {
+                             required: true, 
+                             // Regex to validate email 
+                             pattern: /^[A-Z-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+                         })}
+                         />
+                    </form>
+                </motion.div>
+            </div>
         </section>
     )
 }
