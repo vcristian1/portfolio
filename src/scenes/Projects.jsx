d2c94f3b93eab248c4f28d2e8d192e2415fd08ac
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-26 12:34:33 -0500

    adjusted initial value on motion.div for projects so it is shown since the animation doesnt show on mobile and keeps it hidden

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -78,7 +78,7 @@ export const Projects = () => {
                 viewport={{ once: true, amount: 0.5 }}
                 transition={{ delay: 0.4, duration: 0.5 }}
                 variants={{
-                 hidden: { opacity: 0, x: -50 },
+                 hidden: { opacity: 0.5, x: -50 },
                  visible: { opacity: 1, x: 0 },
                 }}
                 >
@@ -90,7 +90,7 @@ export const Projects = () => {
         <div className="flex justify-center">
             <motion.div
                 className="xs:grid-cols-1 sm:grid sm:grid-cols-3"
-                initial="hidden"
+                initial="visible"
                 whileInView="visible"
                 viewport={{ once: true, amount: 0.5 }}
                 variants={container}
